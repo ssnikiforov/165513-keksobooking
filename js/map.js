03ba50b9c0865696a2addd2801f54d3d9ff15904
ssnikiforov <stepan001@yandex.ru>
2018-02-16 22:54:44 +0300

    Move data to data.js

--- js/map.js
+++ js/map.js
@@ -1,186 +1,18 @@
 'use strict';
 
 (function () {
-  var NUMBER_OF_ADS = 8;
-
-  var TITLES = [
-    'Большая уютная квартира',
-    'Маленькая неуютная квартира',
-    'Огромный прекрасный дворец',
-    'Маленький ужасный дворец',
-    'Красивый гостевой домик',
-    'Некрасивый негостеприимный домик',
-    'Уютное бунгало далеко от моря',
-    'Неуютное бунгало по колено в воде'
-  ];
-
-  var TYPES = [
-    'flat',
-    'house',
-    'bungalo'
-  ];
-
-  var PRICE_MIN = 1000;
-  var PRICE_MAX = 1000000;
-  var PRICE_MIN_BUNGALO = 0;
-  var PRICE_MIN_FLAT = 1000;
-  var PRICE_MIN_HOUSE = 5000;
-  var PRICE_MIN_PALACE = 10000;
-
-  var ROOMS_MIN = 1;
-  var ROOMS_MAX = 5;
-
-  var GUESTS_MAX = 10;
-
-  var TIME_VALUES = [
-    '12:00',
-    '13:00',
-    '14:00'
-  ];
-
-  var FEATURES = [
-    'wifi',
-    'dishwasher',
-    'parking',
-    'washer',
-    'elevator',
-    'conditioner'
-  ];
-
-  var PHOTOS = [
-    'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-    'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-    'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
-  ];
-
   var X_COORD_MIN = 300;
   var X_COORD_MAX = 900;
   var Y_COORD_MIN = 150;
   var Y_COORD_MAX = 500;
-
-  var MAP_PIN_WIDTH = 62;
-  var MAP_PIN_HEIGHT = 84;
-
   var PIN_INITIAL_X = (X_COORD_MAX - X_COORD_MIN) / 2;
   var PIN_INITIAL_Y = (Y_COORD_MAX - Y_COORD_MIN) / 2;
 
-  var getRandomNumber = function (value) {
-    if (!value) {
-      return value;
-    }
-
-    return Math.floor(Math.random() * value);
-  };
-
-  var getRandomNumberFromRange = function (min, max) {
-    return Math.round(Math.random() * (max - min) + min);
-  };
-
-  var getAvatar = function (number) {
-    number = number < 10 ? '0' + number : number;
-
-    return 'img/avatars/user' + number + '.png';
-  };
-
-  var getRandomIndex = function (array) {
-    return getRandomNumber(array.length);
-  };
-
-  var getTitle = function (titlesArr) {
-    var index = getRandomIndex(titlesArr);
-    var title = titlesArr[index];
-
-    if (index > -1) {
-      titlesArr.splice(index, 1);
-    }
-
-    return title;
-  };
-
-  var getRandomPrice = function (priceMin, priceMax) {
-    return getRandomNumberFromRange(priceMin, priceMax);
-  };
-
-  var getRandomType = function (types) {
-    return types[getRandomIndex(types)];
-  };
-
-  var getRandomRoom = function (roomsMin, roomsMax) {
-    return getRandomNumberFromRange(roomsMin, roomsMax);
-  };
-
-  var getGuestsQuantity = function (guests) {
-    return getRandomNumber(guests);
-  };
-
-  var getRandomCheckInValue = function (checkInTime) {
-    return checkInTime[getRandomIndex(checkInTime)];
-  };
-
-  var getRandomCheckOutValue = function (checkOutTime) {
-    return checkOutTime[getRandomIndex(checkOutTime)];
-  };
-
-  var getRandomFeatures = function (features) {
-    var arr = [];
-    for (var i = 0, n = getRandomIndex(features) || 1; i < n; i++) {
-      arr.push(features[getRandomIndex(features)]);
-    }
-
-    return arr;
-  };
-
-  var getRandomPhotos = function (photos) {
-    var photosTemp = photos.slice();
-
-    var arr = [];
-    for (var i = 0, n = photos.length; i < n; i++) {
-      var index = getRandomIndex(photosTemp);
-      arr.push(photosTemp[index]);
-
-      if (index > -1) {
-        photosTemp.splice(index, 1);
-      }
-    }
-
-    return arr;
-  };
-
-  var getAds = function (numberOfAds) {
-    var ads = [];
-    var titlesTemp = TITLES.slice();
-
-    for (var i = 0; i < numberOfAds; i++) {
-      var xValue = getRandomNumberFromRange(X_COORD_MIN, X_COORD_MAX);
-      var yValue = getRandomNumberFromRange(Y_COORD_MIN, Y_COORD_MAX);
-
-      var ad = {
-        author: {
-          avatar: getAvatar(i + 1)
-        },
-        offer: {
-          title: getTitle(titlesTemp),
-          address: xValue + ', ' + yValue,
-          price: getRandomPrice(PRICE_MIN, PRICE_MAX),
-          type: getRandomType(TYPES),
-          rooms: getRandomRoom(ROOMS_MIN, ROOMS_MAX),
-          guests: getGuestsQuantity(GUESTS_MAX),
-          checkin: getRandomCheckInValue(TIME_VALUES),
-          checkout: getRandomCheckOutValue(TIME_VALUES),
-          features: getRandomFeatures(FEATURES),
-          description: '',
-          photos: getRandomPhotos(PHOTOS)
-        },
-        location: {
-          x: xValue + MAP_PIN_WIDTH / 2,
-          y: yValue + MAP_PIN_HEIGHT
-        }
-      };
-      ads.push(ad);
-    }
-
-    return ads;
-  };
+  var NUMBER_OF_ADS = 8;
+  var PRICE_MIN_BUNGALO = 0;
+  var PRICE_MIN_FLAT = 1000;
+  var PRICE_MIN_HOUSE = 5000;
+  var PRICE_MIN_PALACE = 10000;
 
   var renderMapPin = function (template, ad) {
     var pinElement = template.cloneNode(true);
@@ -318,8 +150,8 @@
 
   var fillAddressField = function () {
     var addressFormField = noticeForm.querySelector('#address');
-    var mainPinX = PIN_INITIAL_X - MAP_PIN_WIDTH / 2;
-    var mainPinY = PIN_INITIAL_Y - MAP_PIN_HEIGHT;
+    var mainPinX = PIN_INITIAL_X - window.pin.width / 2;
+    var mainPinY = PIN_INITIAL_Y - window.pin.height;
 
     addressFormField.value = mainPinX + ', ' + mainPinY;
   };
@@ -328,7 +160,7 @@
   var template = document.querySelector('template').content;
   var noticeForm = document.querySelector('.notice__form');
   var mainPin = map.querySelector('.map__pin--main');
-  var ads = getAds(NUMBER_OF_ADS);
+  var ads = window.data.ads(NUMBER_OF_ADS);
 
   // TODO: remove this two lines later
   // activatePage(true);
@@ -489,4 +321,14 @@
   };
 
   runForm();
+
+  window.map = {
+    randomCoordX: function () {
+      return window.util.getRandomNumberFromRange(X_COORD_MIN, X_COORD_MAX)
+    },
+    randomCoordY: function () {
+      return window.util.getRandomNumberFromRange(Y_COORD_MIN, Y_COORD_MAX)
+    }
+
+  }
 })();
--- js/util.js
+++ js/util.js
@@ -1,5 +1,25 @@
 'use strict';
 
 (function () {
+  var getRandomNumber = function (value) {
+    if (!value) {
+      return value;
+    }
 
+    return Math.floor(Math.random() * value);
+  };
+
+  var getRandomNumberFromRange = function (min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+  };
+
+  var getRandomIndex = function (array) {
+    return getRandomNumber(array.length);
+  };
+
+  window.util = {
+    getRandomNumber: getRandomNumber,
+    getRandomIndex: getRandomIndex,
+    getRandomNumberFromRange: getRandomNumberFromRange
+  };
 })();
