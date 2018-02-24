9211051df592cc282ed5d97df7271016e0f2e0f1
ssnikiforov <stepan001@yandex.ru>
2018-02-24 17:09:36 +0300

    Add default success and error handlers into util.js

--- js/util.js
+++ js/util.js
@@ -1,6 +1,10 @@
 'use strict';
 
 (function () {
+  var SUBMIT_SUCCESS_MESSAGE = 'Данные формы были успешно сохранены';
+  var ALERT_SHOW_TIME = 3000;
+  var ALERT_SHOW_STEPS = 50;
+
   var getRandomNumber = function (value) {
     if (!value) {
       return value;
@@ -17,9 +21,61 @@
     return getRandomNumber(array.length);
   };
 
+  var getAlertMessageNode = function (type) {
+    var stylesArr = [
+      'z-index: 100;',
+      'margin: 0 auto;',
+      'padding: 30px;',
+      'position: absolute;',
+      'left: 0;',
+      'right: 0;',
+      'width: 100%;',
+      'text-align: center;',
+      'font-size: 24px;',
+      'opacity: 1;'
+    ];
+    stylesArr.push(type === 'success' ? 'background-color: green;' : 'background-color: red;');
+
+    var node = document.createElement('div');
+    node.style = stylesArr.join(' ');
+
+    return node;
+  };
+
+  var hideAlert = function (alert) {
+    if (!alert) {
+      return;
+    }
+    var currentOpacity = alert.style.opacity;
+    var timer = setInterval(function () {
+      currentOpacity -= 1 / ALERT_SHOW_STEPS;
+      if (currentOpacity <= 0) {
+        clearInterval(timer);
+        document.body.removeChild(alert);
+      }
+      alert.style.opacity = currentOpacity;
+    }, ALERT_SHOW_TIME / ALERT_SHOW_STEPS);
+  };
+
+  var successHandler = function () {
+    var alertNode = getAlertMessageNode('success');
+    alertNode.textContent = SUBMIT_SUCCESS_MESSAGE;
+    document.body.insertAdjacentElement('afterbegin', alertNode);
+    hideAlert(alertNode);
+  };
+
+  var errorHandler = function (errorMessage) {
+    var alertNode = getAlertMessageNode('error');
+    alertNode.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', alertNode);
+    hideAlert(alertNode);
+  };
+
   window.util = {
     getRandomNumber: getRandomNumber,
     getRandomIndex: getRandomIndex,
-    getRandomNumberFromRange: getRandomNumberFromRange
+    getRandomNumberFromRange: getRandomNumberFromRange,
+    successHandler: successHandler,
+    errorHandler: errorHandler
   };
 })();
